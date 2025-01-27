dde002978f5b3bf292ddc9336b83d4bcc94f692d
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-01-27 22:34:21 +0200

    add bg-image from home-page, add fonts

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,18 @@
+import CommonButton from '../../components/CommonButton/CommonButton.jsx';
+
 const HomePage = () => {
-  return <div className='text-text-light'>HomePage</div>;
+  return (
+    <div className="bg-[url('/assets/img/truck_bg.jpg')]  bg-cover bg-center bg-no-repeat px-[64px] py-[260px] m-auto max-w-[1440px] h-[696px] bg-retina">
+      <div className='h-[176px]'>
+        <h1 className='text-gray-100 text-[48px] font-semibold leading-[32px] mb-4'>
+          Campers of your dreams
+        </h1>
+        <p className='text-gray-100  text-[24px] leading-[32px] font-semibold mb-[40px]'>
+          You can find everything you want in our catalog
+        </p>
+        <CommonButton value={'View No'} redirect={'catalog'} />
+      </div>
+    </div>
+  );
 };
 export default HomePage;
